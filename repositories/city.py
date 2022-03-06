14fb75ff15814478cc004c4ee80d7bd7d200cd80
antonioliverjr <antoniobatistajr@gmail.com>
2022-03-06 10:49:56 -0300

    Criando repositorios para manipulação das models, desacoplando a a entidade do controller

--- repositories/city.py
+++ repositories/city.py
@@ -1,23 +1,46 @@
+from typing import Union, List
+from data.context import Context
 from models.city import CityModel
 
 
-class CityRepository(CityModel):
-    @classmethod
-    def list(cls):
-        return cls.query.all()
+class CityRepository():
+    def __init__(self) -> None:
+        self.conn = Context()
+
+    def list(self) -> List[CityModel]:
+        return self.conn.session.query(CityModel).all()
+
+    def list_id(self, id:int) -> Union[CityModel, None]:
+        return self.conn.session.query(CityModel).filter_by(id=id).first()
+
+    def create(self, name:str, uf:str) -> Union[CityModel, Exception]:
+        city = CityModel(name.upper(), uf.upper())
+        try:
+            self.conn.session.add(city)
+            self.conn.save()
+        except Exception as ex:
+            return ex
+
+        result = self.search(name)       
+        return result
+
+    def update(self, id:int, **city) -> Union[CityModel, Exception]:
+        city['name'] = city['name'].upper()
+        city['uf'] = city['uf'].upper()
+        try:
+            self.conn.session.query(CityModel).filter_by(id=id).update(city, synchronize_session=False)
+            self.conn.save()
+        except Exception as ex:
+            return ex
+        return self.list_id(id)
     
-    def list_id(self):
-        pass
-    
-    def create(self, **Kwargs):
-        pass
+    def delete(self, id:int) -> Union[bool, Exception]:
+        try:
+            self.conn.session.query(CityModel).filter_by(id=id).delete(synchronize_session=False)
+            self.conn.save()
+        except Exception as ex:
+            return ex
+        return True
 
-    def uptade(self):
-        pass
-
-    def delete(self):
-        pass
-
-    @classmethod
-    def search(cls, city:str):
-        return cls.query.filter_by(name=city).first()
+    def search(self, city_name:str) -> Union[CityModel, None]:
+        return self.conn.session.query(CityModel).filter_by(name=city_name.upper()).first()
--- repositories/hotel.py
+++ repositories/hotel.py
@@ -1,20 +1,24 @@
-from typing import List
+from typing import Union, List
+from data.context import Context
 from models.hotel import HotelModel
 from models.city import CityModel
 
 
-class HotelRepository(HotelModel):
-    def list(self):
-        pass
+class HotelRepository():
+    def __init__(self) -> None:
+        self.conn = Context()
     
-    def list_id(self):
-        pass
+    def list(self) -> List[HotelModel]:
+        return self.conn.session.query(HotelModel).all()
     
-    def create(cls, name:str, stars:int, daily:float, cities:List[CityModel]):
-        pass
+    def list_id(self, id:int) -> Union[HotelModel, None]:
+        return self.conn.session.query(HotelModel).filter_by(id=id).first()
 
-    def uptade(self):
-        pass
-
-    def delete(self):
-        pass
+    def create(self, name:str, stars:int, daily:float, city:CityModel):
+        hotel = HotelModel(name, stars, daily, city_id=city)
+        try:
+            self.conn.session.add(hotel)
+            self.conn.save()
+        except Exception as ex:
+            return ex
+        return hotel
